176c6c9c73a486f94bd5fb6c7b8895d28f4961c0
elenaAeternanox <antares87@mail.ru>
2021-11-30 10:30:28 +0100

    New project for bot

---
+++ src/test/java/com/github/elenaAeternanox/twelfthHomeWork/pages/components/SubjectComponent.java
@@ -0,0 +1,31 @@
+package com.github.elenaAeternanox.twelfthHomeWork.pages.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.github.elenaAeternanox.twelfthHomeWork.tests.TestData.*;
+
+public class SubjectComponent {
+
+    private SelenideElement subjectSelector = $("#subjectsInput");
+
+    public String setSubjects(int subjectNumber) {
+        String subjectsList = "";
+        switch (subjectNumber) {
+            case 1:
+                subjectSelector.setValue(firstSubjectPrefix);
+                $(byText(firstSubject)).click();
+                subjectsList = firstSubject;
+                break;
+            case 2:
+                subjectSelector.setValue(firstSubjectPrefix);
+                $(byText(firstSubject)).click();
+                subjectSelector.setValue(secondSubjectPrefix);
+                $(byText(secondSubject)).click();
+                subjectsList = firstSubject + ", " + secondSubject;
+                break;
+        }
+        return subjectsList;
+    }
+}
---
+++ src/test/java/com/github/elenaAeternanox/twelfthHomeWork/tests/TestBase.java
@@ -0,0 +1,48 @@
+package com.github.elenaAeternanox.twelfthHomeWork.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.github.elenaAeternanox.twelfthHomeWork.config.EnvironmentConfig;
+import com.github.elenaAeternanox.twelfthHomeWork.helpers.Attach;
+import com.github.elenaAeternanox.twelfthHomeWork.pages.RegistrationPage;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Configuration.*;
+
+public class TestBase {
+
+    public static EnvironmentConfig credentials = ConfigFactory.create(EnvironmentConfig.class);
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    private static String login = credentials.login();
+    private static String password = credentials.password();
+    private static String selenoidUrl = System.getProperty("URL");
+
+    private static String remoteUrl = String.format("https://%s:%s@%s", login, password, selenoidUrl);
+
+    @BeforeAll
+    public static void setup() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        startMaximized = true;
+        remote = remoteUrl;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
